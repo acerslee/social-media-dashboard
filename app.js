adf11598b4b1baa9c3e45660d1e14788a751085d
Alex Lee <ahjlee712@gmail.com>
2021-01-11 18:08:31 -0500

    Creating object constructors

---
+++ app.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+  function overviewBox (title, platform, views, change) {
+    return {
+      title,
+      platform,
+      views,
+      change
+    }
+  }
+
+  const facebookViews = overviewBox('Page Views', 'facebook', 87, '3%');
+
+
+  // const facebookViews = {
+  //   title: 'Page Views',
+  //   views: 87,
+  //   change: '3%'
+  // }
+
+  const facebookLikes = {
+    title: 'Likes',
+        views: 52,
+    change: '2%'
+  }
+
+  const instaLikes = {
+    title: 'Likes',
+    views: 5462,
+    change: '2257%'
+  }
+
+  const instaViews = {
+    title: 'Profile Views',
+    views: '52k',
+    change: '1375%'
+  }
+
+  const retweets = {
+    title: 'Retweets',
+    views: 5462,
+    change: '2257%'
+  }
+
+  // const instaLikes = {
+  //   title: 'Likes',
+  //   views: 5462,
+  //   change: '2257%'
+  // }
+
+  // const instaLikes = {
+  //   title: 'Likes',
+  //   views: 5462,
+  //   change: '2257%'
+  // }
+
+  // const instaLikes = {
+  //   title: 'Likes',
+  //   views: 5462,
+  //   change: '2257%'
+  // }
+
+})
