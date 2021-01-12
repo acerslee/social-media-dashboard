320811cc82dc081759d1f690d126c6af001bdb29
Alex Lee <ahjlee712@gmail.com>
2021-01-11 22:15:27 -0500

    Added OOP for overview dash

--- app.js
+++ app.js
@@ -1,63 +1,51 @@
 $(document).ready(function() {
 
-  function overviewBox (title, platform, views, change) {
-    return {
-      title,
-      platform,
-      views,
-      change
-    }
+  // factory function
+  function OverviewBox (title, platform, views, change) {
+    this.title = title;
+    this.platform = platform;
+    this.views = views;
+    this.change = change;
   }
 
-  const facebookViews = overviewBox('Page Views', 'facebook', 87, '3%');
+  //function to append the info into HTML
+  function appendInfo (object) {
+    var $titleBox = $('<p class = "title"></p>');
+    // var $icon = $();
+    var $numbers = $('<p class = "numbers"></p>');
+    var $changeRange = $('<p class = "range"></p>');
 
-
-  // const facebookViews = {
-  //   title: 'Page Views',
-  //   views: 87,
-  //   change: '3%'
-  // }
-
-  const facebookLikes = {
-    title: 'Likes',
-        views: 52,
-    change: '2%'
+    $titleBox
+      .text(object.title)
+      .appendTo('#overview-dashboard');
+    $numbers
+      .text(object.views)
+      .appendTo('#overview-dashboard');
+    $changeRange
+      .text(object.change)
+      .appendTo('#overview-dashboard');
   }
 
-  const instaLikes = {
-    title: 'Likes',
-    views: 5462,
-    change: '2257%'
+  //function to add media icon according to platform
+  function platformDecider() {
+
   }
 
-  const instaViews = {
-    title: 'Profile Views',
-    views: '52k',
-    change: '1375%'
-  }
-
-  const retweets = {
-    title: 'Retweets',
-    views: 5462,
-    change: '2257%'
-  }
-
-  // const instaLikes = {
-  //   title: 'Likes',
-  //   views: 5462,
-  //   change: '2257%'
-  // }
-
-  // const instaLikes = {
-  //   title: 'Likes',
-  //   views: 5462,
-  //   change: '2257%'
-  // }
-
-  // const instaLikes = {
-  //   title: 'Likes',
-  //   views: 5462,
-  //   change: '2257%'
-  // }
+  const facebookViews = new OverviewBox('Page Views', 'facebook', 87, '3%');
+  appendInfo(facebookViews);
+  const facebookLikes = new OverviewBox('Likes', 'facebook', 52, '2%');
+  appendInfo(facebookLikes);
+  const instaLikes = new OverviewBox('Likes', 'instagram', 5462, '2257%');
+  appendInfo(instaLikes);
+  const instaViews = new OverviewBox('Profile Views', 'instagram', '52k', '1375%');
+  appendInfo(instaViews);
+  const retweets = new OverviewBox('Retweets', 'twitter', 117, '303%');
+  appendInfo(retweets);
+  const tweetLikes = new OverviewBox('Likes', 'twitter', 507, '503%');
+  appendInfo(tweetLikes);
+  const youtubeLikes = new OverviewBox('Likes', 'youtube', 107, '19%');
+  appendInfo(youtubeLikes);
+  const youtubeViews = new OverviewBox('Total Views', 'youtube', 1407, '12%');
+  appendInfo(youtubeViews);
 
 })
